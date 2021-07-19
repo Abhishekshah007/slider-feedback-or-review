5d6b357f1ed929aaeda7b274a5d32fe6ea81f83f
Abhishek shah <52561897+Abhishekshah007@users.noreply.github.com>
2021-07-19 22:21:25 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+const people = [
+    {
+      img:
+        "p1.jpg",
+      name: "Forest",
+      job: "Amazon",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+    },
+    {
+      img:
+        "p2.jpg",
+      name: "Waterfall",
+      job: "India",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+    {
+      img:
+        "p3.jpg",
+      name: "Mountains",
+      job: "Alaska",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+  ];
+  
+  export default people;
